463812439d90711d6522d4fc7e98f194dbec8a81
Karen <karen.bandong.2018@student.lhschools.org>
2019-04-06 19:59:21 -0400

    variables put in

--- src/org/lickingheights/Main.java
+++ src/org/lickingheights/Main.java
@@ -5,7 +5,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner keyboard = new Scanner(System.in);
+        int month, year, day;
+        boolean running = true;
+        while (running = true) 
 
     }
 }
