e66ee8c2eac78b26e77d7fd430efb4ffd9c103ce
Karen <karen.bandong.2018@student.lhschools.org>
2019-04-06 20:17:37 -0400

    debugged

--- src/org/lickingheights/Main.java
+++ src/org/lickingheights/Main.java
@@ -9,7 +9,7 @@ public class Main {
         Scanner keyboard = new Scanner(System.in);
         int month, year, day;
         boolean running = true;
-        while (running = true)
+        while (running = true) {
             System.out.println("Monday's child is fair of face,\n" +
                     "Tuesday's child is full of grace,\n" +
                     "Wednesday's child is full of woe,\n" +
@@ -18,97 +18,98 @@ public class Main {
                     "Saturday's child works hard for a living,\n" +
                     "But the child born on the Sabbath Day,\n" +
                     "Is fair and wise and good in every way.\n");
-        System.out.println("What month were you born?");
-        month = keyboard.nextInt();
-        System.out.println("What day were you born?");
-        day = keyboard.nextInt();
-        System.out.println("What year were you born?");
-        year = keyboard.nextInt();
+            System.out.println("What month were you born?");
+            month = keyboard.nextInt();
+            System.out.println("What day were you born?");
+            day = keyboard.nextInt();
+            System.out.println("What year were you born?");
+            year = keyboard.nextInt();
 
-        daysofmonth(month);
-        dayoftheweek(day, month, year);
+            daysofmonth(month);
+            dayoftheweek(day, month, year);
 
-    }
-
-    public static String daysofmonth(int month) {
-        String monthString = "";
-        switch (month) {
-            case 1:
-                monthString = "There are 31 days in January";
-                break;
-            case 2:
-                monthString = "There are either 28 days or 29 days in February";
-                break;
-            case 3:
-                monthString = "There are 31 days in March";
-                break;
-            case 4:
-                monthString = "There are 30 days in April";
-                break;
-            case 5:
-                monthString = "There are 31 days in May";
-                break;
-            case 6:
-                monthString = "There are 30 days in June";
-                break;
-            case 7:
-                monthString = "There are 31 days in July";
-                break;
-            case 8:
-                monthString = "There are 30 days in August";
-                break;
-            case 9:
-                monthString = "There are 31 days in September";
-                break;
-            case 10:
-                monthString = "There are 30 days in October";
-                break;
-            case 11:
-                monthString = "There are 31 days in November";
-                break;
-            case 12:
-                monthString = "There are 30 days in December";
-                break;
         }
-        System.out.println(monthString);
-        return (monthString);
     }
 
-    public static String dayoftheweek(int day, int month, int year) {
-        String daysString = "";
-        int dayoftheweek;
-        int modYear = year % 100;
-        int zeroYear = year / 100;
-        dayoftheweek = day + 13 * (month + 1) / 5 + modYear + modYear / 4 + zeroYear / 4 + 5 * zeroYear;
-        dayoftheweek = dayoftheweek % 7;
-
-        switch (dayoftheweek) {
-
-            case 0:
-                daysString = "Saturday";
-                break;
-            case 1:
-                daysString = "Sunday";
-                break;
-            case 2:
-                daysString = "Monday";
-                break;
-            case 3:
-                daysString = "Tuesday";
-                break;
-            case 4:
-                daysString = "Wednesday";
-                break;
-            case 5:
-                daysString = "Thursday";
-                break;
-            case 6:
-                daysString = "Friday";
-                break;
+        public static String daysofmonth ( int month){
+            String monthString = "";
+            switch (month) {
+                case 1:
+                    monthString = "There are 31 days in January";
+                    break;
+                case 2:
+                    monthString = "There are either 28 days or 29 days in February";
+                    break;
+                case 3:
+                    monthString = "There are 31 days in March";
+                    break;
+                case 4:
+                    monthString = "There are 30 days in April";
+                    break;
+                case 5:
+                    monthString = "There are 31 days in May";
+                    break;
+                case 6:
+                    monthString = "There are 30 days in June";
+                    break;
+                case 7:
+                    monthString = "There are 31 days in July";
+                    break;
+                case 8:
+                    monthString = "There are 30 days in August";
+                    break;
+                case 9:
+                    monthString = "There are 31 days in September";
+                    break;
+                case 10:
+                    monthString = "There are 30 days in October";
+                    break;
+                case 11:
+                    monthString = "There are 31 days in November";
+                    break;
+                case 12:
+                    monthString = "There are 30 days in December";
+                    break;
+            }
+            System.out.println(monthString);
+            return (monthString);
         }
-        System.out.println(daysString);
 
-        return (daysString);
+        public static String dayoftheweek ( int day, int month, int year){
+            String daysString = "";
+            int dayoftheweek;
+            int modYear = year % 100;
+            int zeroYear = year / 100;
+            dayoftheweek = day + 13 * (month + 1) / 5 + modYear + modYear / 4 + zeroYear / 4 + 5 * zeroYear;
+            dayoftheweek = dayoftheweek % 7;
 
+            switch (dayoftheweek) {
+
+                case 0:
+                    daysString = "Saturday";
+                    break;
+                case 1:
+                    daysString = "Sunday";
+                    break;
+                case 2:
+                    daysString = "Monday";
+                    break;
+                case 3:
+                    daysString = "Tuesday";
+                    break;
+                case 4:
+                    daysString = "Wednesday";
+                    break;
+                case 5:
+                    daysString = "Thursday";
+                    break;
+                case 6:
+                    daysString = "Friday";
+                    break;
+            }
+            System.out.println(daysString);
+
+            return (daysString);
+
+        }
     }
-}
